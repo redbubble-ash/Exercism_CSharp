2ab665361b9db82fb76a831f493b38ef9d103470
Jing Xie <xiejing418@gmail.com>
2019-10-18 23:33:29 -0700

    Clock 20 unit tests passed

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -36,6 +36,28 @@ public class Clock
 
     public override string ToString()
     {
+        int addHours = 0;
+        if(Minutes >= 60){
+            addHours = Minutes / 60;
+            Minutes = Minutes % 60;
+        }
+        if(Hours >= 0){
+         Hours = (Hours + addHours) % 24 ;
+        }
+        if(Minutes < 0){
+            if(Minutes % 60 == 0){
+            addHours = Minutes / 60;
+            Minutes = Minutes % 60;
+            } else {
+            addHours = Minutes / 60 - 1;
+            Minutes = 60 + Minutes % 60;
+            }
+            Hours += addHours;
+        }
+        if(Hours < 0){
+            Hours = 24 + Hours % 24;
+        }
+
         return ($"{String.Format("{0:00}", Hours)}:{String.Format("{0:00}", Minutes)}");
     }
 }
