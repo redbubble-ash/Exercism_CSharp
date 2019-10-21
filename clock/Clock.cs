bf35d809527b693f958ddb82bb2cc01a27a835b0
Jing Xie <xiejing418@gmail.com>
2019-10-21 11:41:59 -0700

    passed 38/53 test_clock

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -14,21 +14,21 @@ public class Clock
 
     public Clock Add(int minutesToAdd)
     {
-        Clock clockAdd = new Clock(Hours,Minutes);
-        int convertToMinutes = Hours * 60;
+        Clock clockAdd = new Clock(Hours, Minutes);
+        int convertToMinutes = clockAdd.Hours * 60 + clockAdd.Minutes;
         convertToMinutes += minutesToAdd;
-        Hours = convertToMinutes / 60;
-        Minutes = convertToMinutes % 60;
+        clockAdd.Hours = convertToMinutes / 60;
+        clockAdd.Minutes = convertToMinutes % 60;
         return clockAdd;
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        Clock clockSubtract = new Clock(Hours,Minutes);
-        int convertToMinutes = Hours * 60;
+        Clock clockSubtract = new Clock(Hours, Minutes);
+        int convertToMinutes = clockSubtract.Hours * 60 + Minutes;
         convertToMinutes -= minutesToSubtract;
-        Hours = convertToMinutes / 60;
-        Minutes = convertToMinutes % 60;
+        clockSubtract.Hours = convertToMinutes / 60;
+        clockSubtract.Minutes = convertToMinutes % 60;
         return clockSubtract;
 
 
@@ -37,24 +37,31 @@ public class Clock
     public override string ToString()
     {
         int addHours = 0;
-        if(Minutes >= 60){
+        if (Minutes >= 60)
+        {
             addHours = Minutes / 60;
             Minutes = Minutes % 60;
         }
-        if(Hours >= 0){
-         Hours = (Hours + addHours) % 24 ;
+        if (Hours >= 0)
+        {
+            Hours = (Hours + addHours) % 24;
         }
-        if(Minutes < 0){
-            if(Minutes % 60 == 0){
-            addHours = Minutes / 60;
-            Minutes = Minutes % 60;
-            } else {
-            addHours = Minutes / 60 - 1;
-            Minutes = 60 + Minutes % 60;
+        if (Minutes < 0)
+        {
+            if (Minutes % 60 == 0)
+            {
+                addHours = Minutes / 60;
+                Minutes = Minutes % 60;
+            }
+            else
+            {
+                addHours = Minutes / 60 - 1;
+                Minutes = 60 + Minutes % 60;
             }
             Hours += addHours;
         }
-        if(Hours < 0){
+        if (Hours < 0)
+        {
             Hours = 24 + Hours % 24;
         }
 
