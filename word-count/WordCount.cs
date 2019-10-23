41c18de83fc096230efc05552397f2140de63df5
Jing Xie <xiejing418@gmail.com>
2019-10-23 15:58:25 -0700

    passed 11 Word_Count

--- word-count/WordCount.cs
+++ word-count/WordCount.cs
@@ -1,16 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Collections;
+using System.Text.RegularExpressions;
+
 public static class WordCount
 {
     public static IDictionary<string, int> CountWords(string phrase)
     {
+        //[   #Character block start.
+        //^   #Not these characters (letters, numbers).
+        //\w  #Word characters.
+        //\s  #Space characters.
+        //]   #Character block end.
+        string newPhrase = Regex.Replace(phrase, @"[^\w ']", " ").ToLower();
+        Console.WriteLine("Regex output is " + newPhrase);
+        //\w?'\w?
         IDictionary<string, int> wordDictionary = new Dictionary<string, int>();
-        string[] arr = phrase.Split(" ");
-        ArrayList withoutDuplicate = new ArrayList(); 
-        for (int i=0; i< arr.Length; i++)
+
+
+        string[] arr = newPhrase.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+        ArrayList withoutDuplicate = new ArrayList();
+        for (int i = 0; i < arr.Length; i++)
         {
-            if(withoutDuplicate.Contains(arr[i]) == false)
+            if (withoutDuplicate.Contains(arr[i]) == false)
             {
                 withoutDuplicate.Add(arr[i]);
             }
