31c2dfd6bc4bbf015b1bf5e84b9abce59b31284d
Jing Xie <xiejing418@gmail.com>
2019-10-24 15:50:48 -0700

    passed all Triangle

--- triangle/Triangle.cs
+++ triangle/Triangle.cs
@@ -2,18 +2,39 @@ using System;
 
 public static class Triangle
 {
+    public static bool IsTriangle(double side1, double side2, double side3)
+    {
+
+        if (side1 > 0 && side2 > 0 && side3 > 0 && side1 <= (side2 + side3) && side2 <= (side1 + side3) && side3 <= (side1 + side2)) return true;
+        else return false;
+
+    }
     public static bool IsScalene(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (IsTriangle(side1, side2, side3)) {
+            if (side1 != side2 && side1 != side3 && side2 != side3) return true;
+            else return false;
+        }
+        return false;
     }
 
-    public static bool IsIsosceles(double side1, double side2, double side3) 
+    public static bool IsIsosceles(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (IsTriangle(side1, side2, side3))
+        {
+            if ((side1 == side2) || (side1 == side3 ) || (side2 == side3 )) return true;
+            else return false;
+        }
+        return false;
     }
 
-    public static bool IsEquilateral(double side1, double side2, double side3) 
+    public static bool IsEquilateral(double side1, double side2, double side3)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (IsTriangle(side1, side2, side3))
+        {
+            if (side1 == side2 && side2 == side3) return true;
+            else return false;
+        }
+        return false;
     }
 }
