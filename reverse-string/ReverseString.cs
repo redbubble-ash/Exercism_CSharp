fb1f5bb796c7d74ab33d8808c8cde57d9e0a2c7d
Jing Xie <xiejing418@gmail.com>
2019-10-29 22:44:25 -0700

    updated reverse string

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -29,3 +29,15 @@ public static class ReverseString
 
     }
 }
+
+//public static string Reverse(string input)
+//{
+//    var builder = new StringBuilder();
+
+//    foreach (char c in input)
+//    {
+//        builder.Insert(0, c); //the character was inserted at the first position of the string builder.
+//    }
+
+//    return builder.ToString();
+//}
