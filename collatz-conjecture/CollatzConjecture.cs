4e5f5c0694a6093cabc58c5645db406a79a43bad
Jing Xie <xiejing418@gmail.com>
2020-01-07 11:11:22 -0800

    passed all Collatz Conjecture

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        int steps = 0;
+        if (number <= 0 )
+        {
+            throw new ArgumentException();
+        }
+        else
+        {
+            while (number != 1)
+            {
+                switch (number % 2)
+                {
+                    case 0:
+                        number = number / 2;
+                        break;
+                    case 1:
+                        number = 3 * number + 1;
+                        break;
+
+                }
+
+                steps++;
+            }
+        }
+
+        return steps;
+    }
+    
+}
